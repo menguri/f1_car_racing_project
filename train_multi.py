bc871a9c8fca877ba65c43a284e91cddc0cfd720
113bommy <113bommy@gmail.com>
2024-12-01 15:19:26 +0000

    multi gpu correction

--- train_multi.py
+++ train_multi.py
@@ -68,19 +68,18 @@ class CNN(nn.Module):
         super().__init__(*args, **kwargs)
         self._n_features = 32 * 9 * 9
 
-    self.conv = nn.Sequential(
-        nn.Conv2d(in_channels, 16, kernel_size=8, stride=4),
-        nn.ReLU(),
-        nn.Conv2d(16, 32, kernel_size=4, stride=2),
-        nn.ReLU(),
-    )
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, 16, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, kernel_size=4, stride=2),
+            nn.ReLU(),
+        )
 
-    self.fc = nn.Sequential(
-        nn.Linear(self._n_features, 256),
-        nn.ReLU(),
-        nn.Linear(256, out_channels),
-    )
-
+        self.fc = nn.Sequential(
+            nn.Linear(self._n_features, 256),
+            nn.ReLU(),
+            nn.Linear(256, out_channels),
+        )
 
     def forward(self, x):
         x = self.conv(x)
