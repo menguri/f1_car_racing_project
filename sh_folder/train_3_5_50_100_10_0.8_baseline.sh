cb7e395b41a4ee79a686a5833d96495b9af0bb45
menguri <alsrn50@gate1.hpc>
2025-02-14 09:59:27 +0900

    car project updated final

---
+++ sh_folder/train_3_5_50_100_10_0.8_baseline.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --partition=gpu1 
+#SBATCH --cpus-per-task=48
+#SBATCH --gres=gpu:4
+#SBATCH --job-name=MINGK    
+#SBATCH -o ./jupyter_output/jupyter.%N.%j.out  # STDOUT ( Standard Output ) 
+#SBATCH -e ./jupyter_output/jupyter.%N.%j.err  # STDERR ( Standard Error ) 
+
+echo "start at:" `date`
+echo "node: $HOSTNAME"
+echo "jobid: $alsrn5051"
+
+module python/3.11.2
+
+# 파라미터 값 정의
+TU=3
+TC=5
+TR=50
+MAP_RESET_INTERVAL=100
+REPLAY_RATIO=10
+EPISODES=1000
+ALPHA=0.8
+SAVE_POINT=100
+ENV_COUNT=4
+MAX_TIMESTEP=900
+
+# train.py 실행 시 파라미터 전달
+python ../model_train.py --TU $TU \
+                  --TC $TC \
+                  --TR $TR \
+                  --map_reset_interval $MAP_RESET_INTERVAL \
+                  --replay_ratio $REPLAY_RATIO \
+                  --episodes $EPISODES \
+                  --alpha $ALPHA \
+                  --save_point $SAVE_POINT \
+                  --env_count $ENV_COUNT \
+                  --max_timestep $MAX_TIMESTEP
---
+++ sh_folder/train_3_5_50_10_0.9_100_speed30_Constrained.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --partition=gpu1 
+#SBATCH --cpus-per-task=48
+#SBATCH --gres=gpu:4
+#SBATCH --job-name=MINGK    
+#SBATCH -o ./jupyter_output/jupyter.%N.%j.out  # STDOUT ( Standard Output ) 
+#SBATCH -e ./jupyter_output/jupyter.%N.%j.err  # STDERR ( Standard Error ) 
+
+echo "start at:" `date`
+echo "node: $HOSTNAME"
+echo "jobid: $alsrn5051"
+
+module python/3.11.2
+
+# Baseline 파라미터 값 정의
+TU=3
+TC=5
+TR=50
+MAP_RESET_INTERVAL=100
+REPLAY_RATIO=10
+EPISODES=1000
+ALPHA=0.9
+SAVE_POINT=100
+ENV_COUNT=4
+MAX_TIMESTEP=900
+
+## Constrained RL
+CONSTRAINED_USE=1
+MAX_SPEED=30
+
+# train.py 실행 시 파라미터 전달
+python ../model_train.py --TU $TU \
+                  --TC $TC \
+                  --TR $TR \
+                  --map_reset_interval $MAP_RESET_INTERVAL \
+                  --replay_ratio $REPLAY_RATIO \
+                  --episodes $EPISODES \
+                  --alpha $ALPHA \
+                  --save_point $SAVE_POINT \
+                  --env_count $ENV_COUNT \
+                  --max_timestep $MAX_TIMESTEP \
+                  --constrained_use $CONSTRAINED_USE \
+                  --max_speed $MAX_SPEED
